dd4b75393131e2bc183d2275a0485c7899aed05a
Stan Chen <sand.chen19@gmail.com>
2013-01-26 22:06:44 +1100

    SwitchView example
    switch view example

---
+++ SwitchViewExample/gen/org/Stan/SwitchView/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.Stan.SwitchView;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ SwitchViewExample/src/org/Stan/Scroll/OnViewChangeListener.java
@@ -0,0 +1,5 @@
+package org.Stan.Scroll;
+
+public interface OnViewChangeListener {
+	public void OnViewChange(int view);
+}
